fc721016742ec4ff91de7c8b167a2d3b0e8cbe32
ishiduca <ishiduca@gmail.com>
2018-06-17 12:45:30 +0900

    サムネイルのURLを変更

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ Melonbooks.prototype.transformQuery = function (_p) {
     gnr: 'genre',
     mch: 'chara',
     com: 'detail',
-//    ser: 'all',
+    // ser: 'all',
     kyw: 'tag'
   }
   var p = xtend(_p)
@@ -38,20 +38,22 @@ Melonbooks.prototype.transformQuery = function (_p) {
     search_disp: '',
     chara: '',
     orderby: 'date', // ["date":新着順,"popular":人気順,"price_desc":価格の高い
-                     // 順,"price_asc":価格の安い順]
+    //                  順,"price_asc":価格の安い順]
     disp_number: 200,
     pageno: 1,
+    fromagee_flg: 2, // 0: melonbooks, 1: Fromagee, 2: 両方
+    // search_target: 0, // 0: すべての商品, 1: 通常の商品, 2: 電子書籍のみ
     text_type: 'all', // ["all":全て,"title":作品タイトル,"detail":作品詳細,"circle":サークル,"maker":メーカー
-                      // "author":作家名,"genre":ジャンル名,"chara":キャラ名,"tag":タグ名,"event":イベント]
-                      // name: '', // 検索キーワード必須
+    //                   "author":作家名,"genre":ジャンル名,"chara":キャラ名,"tag":タグ名,"event":イベント]
+    //                   name: '', // 検索キーワード必須
     product_type: 'all', // ["all":全商品,"st":発売中,"sa":予約受け付け中,"ty":
-                      // お取り寄せ]
+    //                       お取り寄せ]
     'is_end_of_sale[]': 1, // 売り切れ作品を表示
     is_end_of_sale2: 1, // 売り切れ作品を表示
     'category_ids[]': 1 // アイテム[1:同人誌,2:同人ソフト,3:同人グッズ,4:コミッ
-                        // クス,5:ゲーム,6:音楽,17:映像
-                        // 7:商業グッズ,8:自社グッズ]
-     // "additional[]": '' // オプション絞込["pr":特典付き作品,"mn":専売作品,"r18":R18,"r15":R15,"common":一般]
+    //                     クス,5:ゲーム,6:音楽,17:映像
+    //                     7:商業グッズ,8:自社グッズ]
+    // "additional[]": '' // オプション絞込["pr":特典付き作品,"mn":専売作品,"r18":R18,"r15":R15,"common":一般]
   }, q, p)
 }
 
--- t/01.js
+++ t/01.js
@@ -1,46 +1,28 @@
 'use strict'
-var test = require('tape')
-var melonbooks = require('../index')
-var fs = require('fs')
-var path = require('path')
-var iconv = require('iconv-lite')
-var missi = require('mississippi')
+const test = require('tape')
+const melonbooks = require('../index')
+const fs = require('fs')
+const path = require('path')
+const iconv = require('iconv-lite')
+const {pipe, through, concat} = require('mississippi')
 
-test('var client = new Melonbooks', t => {
-  var c = melonbooks()
+test('const client = new Melonbooks', t => {
+  const c = melonbooks()
   t.is(c.name, 'www.melonbooks.co.jp', 'c.name eq "www.melonbooks.co.jp"')
   t.is(c.failAfter, 7, 'c.failAfter === 7')
   t.is(c.searchHome, 'https://www.melonbooks.co.jp/search/search.php','c.search eq "https://www.melonbooks.co.jp/search/search.php"')
   t.end()
 })
 
-test('var qstr = client.createQuery(paarms)', t => {
-  var c = melonbooks()
-  var params = {
+test('const qstr = client.createQuery(paarms)', t => {
+  const c = melonbooks()
+  const params = {
     category: 'mak',
     value: '床子屋'
   }
-// https://www.melonbooks.co.jp/search/search.php
-// mode=search
-// search_disp=
-// chara=
-// orderby=
-// disp_number=
-// pageno=1
-// text_type=circle
-// name=%E5%BA%8A%E5%AD%90%E5%B1%8B
-// is_end_of_sale%5B%5D=1
-// is_end_of_sale2=1
-// co_name=
-// ci_name=
-// sale_date_before=
-// sale_date_after=
-// publication_date_before=
-// publication_date_after=
-// price_low=0
-// price_high=0
-  t.test('var queryObj = client.transformQuery(params)', tt => {
-    var o = c.transformQuery(params)
+
+  t.test('const queryObj = client.transformQuery(params)', tt => {
+    const o = c.transformQuery(params)
     t.deepEqual(o, {
       mode: 'search',
       search_disp: '',
@@ -48,6 +30,7 @@ test('var qstr = client.createQuery(paarms)', t => {
       orderby: 'date',
       disp_number: 200,
       pageno: 1,
+      fromagee_flg: 2,
       text_type: 'circle',
       name: '床子屋',
       'is_end_of_sale[]': 1,
@@ -58,156 +41,41 @@ test('var qstr = client.createQuery(paarms)', t => {
     tt.end()
   })
 
-  var qs = c.createQuery(params)
+  const qs = c.createQuery(params)
   t.ok(/name=%E5%BA%8A%E5%AD%90%E5%B1%8B/.test(qs), qs)
   t.end()
 })
 
-test('var stream = client.scraper()', t => {
-  var c = melonbooks()
-  var b = []
-  var a = [
-    {title: 'ぼくの妹の処女買いませんか',
-    urlOfTitle: 'https://www.melonbooks.co.jp/detail/detail.php?product_id=219583',
-    srcOfThumbnail: 'https:///cdn.melonbooks.co.jp/user_data/packages/resize_image.php?image=212001114806.jpg&width=319&height=450&c=1&aa=0',
-    circle: '床子屋',
-    urlOfCircle: 'https://www.melonbooks.co.jp/circle/index.php?circle_id=19745' },
-    { title: '暗い家',
-    urlOfTitle: 'https://www.melonbooks.co.jp/detail/detail.php?product_id=218594',
-    srcOfThumbnail: 'https:///cdn.melonbooks.co.jp/user_data/packages/resize_image.php?image=212001114282k.jpg&width=319&height=450&c=1&aa=0',
-    circle: '床子屋',
-    urlOfCircle: 'https://www.melonbooks.co.jp/circle/index.php?circle_id=19745' },
-    { title: 'えりかのなつやすみ',
-    urlOfTitle: 'https://www.melonbooks.co.jp/detail/detail.php?product_id=137360',
-    srcOfThumbnail: 'https:///cdn.melonbooks.co.jp/user_data/packages/resize_image.php?image=212001088955.jpg&width=319&height=450&c=1&aa=0',
-    circle: '床子屋',
-    urlOfCircle: 'https://www.melonbooks.co.jp/circle/index.php?circle_id=19745' },
-    { title: '小夜子の食卓【再販分】',
-    urlOfTitle: 'https://www.melonbooks.co.jp/detail/detail.php?product_id=118145',
-    srcOfThumbnail: 'https:///cdn.melonbooks.co.jp/user_data/packages/resize_image.php?image=212001081989.jpg&width=319&height=450&c=1&aa=0',
-    circle: '床子屋',
-    urlOfCircle: 'https://www.melonbooks.co.jp/circle/index.php?circle_id=19745' },
-    { title: 'AFTER THE END',
-    urlOfTitle: 'https://www.melonbooks.co.jp/detail/detail.php?product_id=179526',
-    srcOfThumbnail: 'https:///cdn.melonbooks.co.jp/user_data/packages/resize_image.php?image=212001102625.jpg&width=319&height=450&c=1&aa=0',
-    circle: '床子屋',
-    urlOfCircle: 'https://www.melonbooks.co.jp/circle/index.php?circle_id=19745' },
-    { title: '超どたんばせとぎわ崖っぷち',
-    urlOfTitle: 'https://www.melonbooks.co.jp/detail/detail.php?product_id=21227',
-    srcOfThumbnail: 'https:///cdn.melonbooks.co.jp/user_data/packages/resize_image.php?image=212001069667.jpg&width=319&height=450&c=1&aa=0',
-    circle: '床子屋',
-    urlOfCircle: 'https://www.melonbooks.co.jp/circle/index.php?circle_id=19745' },
-    { title: 'Saint Foire Festival 9 Anessa',
-    urlOfTitle: 'https://www.melonbooks.co.jp/detail/detail.php?product_id=16857',
-    srcOfThumbnail: 'https:///cdn.melonbooks.co.jp/user_data/packages/resize_image.php?image=212001058535.jpg&width=319&height=450&c=1&aa=0',
-    circle: '床子屋',
-    urlOfCircle: 'https://www.melonbooks.co.jp/circle/index.php?circle_id=19745' },
-    { title: 'Saint Foire Festival/eve Evelyn',
-    urlOfTitle: 'https://www.melonbooks.co.jp/detail/detail.php?product_id=16330',
-    srcOfThumbnail: 'https:///cdn.melonbooks.co.jp/user_data/packages/resize_image.php?image=212001054284.jpg&width=319&height=450&c=1&aa=0',
-    circle: '床子屋',
-    urlOfCircle: 'https://www.melonbooks.co.jp/circle/index.php?circle_id=19745' },
-    { title: 'Saint Foire Festival/eve OLWEN:2',
-    urlOfTitle: 'https://www.melonbooks.co.jp/detail/detail.php?product_id=15970',
-    srcOfThumbnail: 'https:///cdn.melonbooks.co.jp/user_data/packages/resize_image.php?image=212001048713.jpg&width=319&height=450&c=1&aa=0',
-    circle: '床子屋',
-    urlOfCircle: 'https://www.melonbooks.co.jp/circle/index.php?circle_id=19745' },
-    { title: 'エルフの女騎士の受難',
-    urlOfTitle: 'https://www.melonbooks.co.jp/detail/detail.php?product_id=151076',
-    srcOfThumbnail: 'https:///cdn.melonbooks.co.jp/user_data/packages/resize_image.php?image=212001094399.jpg&width=319&height=450&c=1&aa=0',
-    circle: '床子屋',
-    urlOfCircle: 'https://www.melonbooks.co.jp/circle/index.php?circle_id=19745' },
-    { title: 'どたんばせとぎわ崖っぷち30',
-    urlOfTitle: 'https://www.melonbooks.co.jp/detail/detail.php?product_id=31422',
-    srcOfThumbnail: 'https:///cdn.melonbooks.co.jp/user_data/packages/resize_image.php?image=212001076650.jpg&width=319&height=450&c=1&aa=0',
-    circle: '床子屋',
-    urlOfCircle: 'https://www.melonbooks.co.jp/circle/index.php?circle_id=19745' },
-    { title: 'Saint Foire Festival/eve Nest',
-    urlOfTitle: 'https://www.melonbooks.co.jp/detail/detail.php?product_id=31404',
-    srcOfThumbnail: 'https:///cdn.melonbooks.co.jp/user_data/packages/resize_image.php?image=212001076632.jpg&width=319&height=450&c=1&aa=0',
-    circle: '床子屋',
-    urlOfCircle: 'https://www.melonbooks.co.jp/circle/index.php?circle_id=19745' },
-    { title: 'Saint Foire Festival/eve Richildis:1&amp;2',
-    urlOfTitle: 'https://www.melonbooks.co.jp/detail/detail.php?product_id=23679',
-    srcOfThumbnail: 'https:///cdn.melonbooks.co.jp/user_data/packages/resize_image.php?image=212001070773.jpg&width=319&height=450&c=1&aa=0',
-    circle: '床子屋',
-    urlOfCircle: 'https://www.melonbooks.co.jp/circle/index.php?circle_id=19745' },
-    { title: 'Saint Foire Festival/eve Richildis:3',
-    urlOfTitle: 'https://www.melonbooks.co.jp/detail/detail.php?product_id=21226',
-    srcOfThumbnail: 'https:///cdn.melonbooks.co.jp/user_data/packages/resize_image.php?image=212001069666.jpg&width=319&height=450&c=1&aa=0',
-    circle: '床子屋',
-    urlOfCircle: 'https://www.melonbooks.co.jp/circle/index.php?circle_id=19745' },
-    { title: 'Saint Foire Festival 10 Matty',
-    urlOfTitle: 'https://www.melonbooks.co.jp/detail/detail.php?product_id=19510',
-    srcOfThumbnail: 'https:///cdn.melonbooks.co.jp/user_data/packages/resize_image.php?image=212001066493.jpg&width=319&height=450&c=1&aa=0',
-    circle: '床子屋',
-    urlOfCircle: 'https://www.melonbooks.co.jp/circle/index.php?circle_id=19745' },
-    { title: 'Saint Foire Festival/eve 総集編',
-    urlOfTitle: 'https://www.melonbooks.co.jp/detail/detail.php?product_id=18086',
-    srcOfThumbnail: 'https:///cdn.melonbooks.co.jp/user_data/packages/resize_image.php?image=212001065317.jpg&width=319&height=450&c=1&aa=0',
-    circle: '床子屋',
-    urlOfCircle: 'https://www.melonbooks.co.jp/circle/index.php?circle_id=19745' },
-    { title: 'えれみか',
-    urlOfTitle: 'https://www.melonbooks.co.jp/detail/detail.php?product_id=17942',
-    srcOfThumbnail: 'https:///cdn.melonbooks.co.jp/user_data/packages/resize_image.php?image=212001064701.jpg&width=319&height=450&c=1&aa=0',
-    circle: '床子屋',
-    urlOfCircle: 'https://www.melonbooks.co.jp/circle/index.php?circle_id=19745' },
-    { title: 'Saint Foire Festival/eve Evelyn:2',
-    urlOfTitle: 'https://www.melonbooks.co.jp/detail/detail.php?product_id=17324',
-    srcOfThumbnail: 'https:///cdn.melonbooks.co.jp/user_data/packages/resize_image.php?image=212001061332.jpg&width=319&height=450&c=1&aa=0',
-    circle: '床子屋',
-    urlOfCircle: 'https://www.melonbooks.co.jp/circle/index.php?circle_id=19745' },
-    { title: 'ALI×MORU 2',
-    urlOfTitle: 'https://www.melonbooks.co.jp/detail/detail.php?product_id=16884',
-    srcOfThumbnail: 'https:///cdn.melonbooks.co.jp/user_data/packages/resize_image.php?image=212001058692.jpg&width=319&height=450&c=1&aa=0',
-    circle: '床子屋',
-    urlOfCircle: 'https://www.melonbooks.co.jp/circle/index.php?circle_id=19745' },
-    { title: 'Saint Foire Festival/eve OLWEN:3',
-    urlOfTitle: 'https://www.melonbooks.co.jp/detail/detail.php?product_id=16433',
-    srcOfThumbnail: 'https:///cdn.melonbooks.co.jp/user_data/packages/resize_image.php?image=212001054936.jpg&width=319&height=450&c=1&aa=0',
-    circle: '床子屋',
-    urlOfCircle: 'https://www.melonbooks.co.jp/circle/index.php?circle_id=19745' },
-    { title: 'ALI×MORU',
-    urlOfTitle: 'https://www.melonbooks.co.jp/detail/detail.php?product_id=16329',
-    srcOfThumbnail: 'https:///cdn.melonbooks.co.jp/user_data/packages/resize_image.php?image=212001054283.jpg&width=319&height=450&c=1&aa=0',
-    circle: '床子屋',
-    urlOfCircle: 'https://www.melonbooks.co.jp/circle/index.php?circle_id=19745' },
-    { title: 'Saint Foire Festival 8 Mabel',
-    urlOfTitle: 'https://www.melonbooks.co.jp/detail/detail.php?product_id=16031',
-    srcOfThumbnail: 'https:///cdn.melonbooks.co.jp/user_data/packages/resize_image.php?image=212001050544.jpg&width=319&height=450&c=1&aa=0',
-    circle: '床子屋',
-    urlOfCircle: 'https://www.melonbooks.co.jp/circle/index.php?circle_id=19745' },
-    { title: 'Saint Foire Festival 7 Mabel',
-    urlOfTitle: 'https://www.melonbooks.co.jp/detail/detail.php?product_id=16021',
-    srcOfThumbnail: 'https:///cdn.melonbooks.co.jp/user_data/packages/resize_image.php?image=212001050385.jpg&width=319&height=450&c=1&aa=0',
-    circle: '床子屋',
-    urlOfCircle: 'https://www.melonbooks.co.jp/circle/index.php?circle_id=19745' },
-    { title: 'Saint Foire Festival 6 Mabel',
-    urlOfTitle: 'https://www.melonbooks.co.jp/detail/detail.php?product_id=15941',
-    srcOfThumbnail: 'https:///cdn.melonbooks.co.jp/user_data/packages/resize_image.php?image=212001047271.jpg&width=319&height=450&c=1&aa=0',
-    circle: '床子屋',
-    urlOfCircle: 'https://www.melonbooks.co.jp/circle/index.php?circle_id=19745' },
-    { title: 'Saint Foire Festival/eve OLWEN',
-    urlOfTitle: 'https://www.melonbooks.co.jp/detail/detail.php?product_id=15896',
-    srcOfThumbnail: 'https:///cdn.melonbooks.co.jp/user_data/packages/resize_image.php?image=212001045639.jpg&width=319&height=450&c=1&aa=0',
-    circle: '床子屋',
-    urlOfCircle: 'https://www.melonbooks.co.jp/circle/index.php?circle_id=19745' }
-  ]
-  var i = 0
+test('const stream = client.scraper()', t => {
+  const c = melonbooks()
+  const html = path.join(__dirname, 'zoal.html')
+  const expected_0 = {
+    title: 'プライベートビーチにて',
+    urlOfTitle: 'https://www.melonbooks.co.jp/detail/detail.php?product_id=374230',
+    circle: 'ZOAL',
+    urlOfCircle: 'https://www.melonbooks.co.jp/circle/index.php?circle_id=9638',
+    srcOfThumbnail: 'https://melonbooks.akamaized.net/user_data/packages/resize_image.php?image=212001159466.jpg&width=319&height=450'
+  }
+  const expected_7 = {
+    title: 'LENAtic5',
+    urlOfTitle: 'https://www.melonbooks.co.jp/detail/detail.php?product_id=17357',
+    circle: 'ZOAL',
+    urlOfCircle: 'https://www.melonbooks.co.jp/circle/index.php?circle_id=9638',
+    srcOfThumbnail: 'https://melonbooks.akamaized.net/user_data/packages/resize_image.php?image=212001061492.jpg&width=319&height=450'
+  }
 
-  missi.pipe(
-    fs.createReadStream(path.join(__dirname, 'tokoya.html')),
+  var buf = []
+
+  pipe(
+    fs.createReadStream(html),
     iconv.decodeStream(c.charset),
     c.scraper(),
-    missi.through.obj((o, _, d) => {
-      b.push(o)
-      t.deepEqual(o, a[i])
-      i += 1
-      d()
-    }),
+    concat(res => (buf = res)),
     err => {
-      t.notOk(err, 'no exits error')
-      t.is(b.length, 25, 'b.length eq 25')
+      t.error(err)
+      t.is(buf.length, 8, 'length 8')
+      t.deepEqual(buf[0], expected_0)
+      t.deepEqual(buf[7], expected_7)
       t.end()
     }
   )
